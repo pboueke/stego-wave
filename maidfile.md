999e2e203b9e29f0dbd39195cc46a80af372a2aa
Pedro Boueke <phboueke@gmail.com>
2018-06-19 20:52:16 -0300

    Parsing .WAV header

--- maidfile.md
+++ maidfile.md
@@ -27,3 +27,11 @@ Run task `build` before this
 ```bash
 ~/.quicklisp/local-projects/stego-wave/stegowave.app "${@:1}"
 ```
+
+## bar:write
+
+Run task `build` before this
+
+```bash
+~/.quicklisp/local-projects/stego-wave/stegowave.app --host=sounds/smashingbaby.wav --message=sounds/test.txt --result=sounds/testresult.wav --write
+```
--- stego-wave.lisp
+++ stego-wave.lisp
@@ -3,7 +3,15 @@
 (in-package #:stego-wave)
 (use-package :apply-argv)
 
-;;; Parameters utilities
+;;; Globals
+
+(defparameter header-size 64)
+
+;;; Macros
+
+(defmacro concat-string (&rest strings) `(concatenate 'string ,@strings) )
+
+;;; Parameters Utils
 
 (defun get-param (keyname params)
     (nth (+ 1 (position keyname params)) params))
@@ -31,15 +39,87 @@
         (error "[ERROR] Missing required parameter ~a" param)))
 
 (defun validate-params (params)
-    (format t "Parsed parameters: ~a ~%" parsedparams)
+    (format t "|* Parsed parameters: ~a ~%" parsedparams)
     (validate-operation params)
     (if (get-flag :write params)
         (validate-write-params params)
         (validate-read-params params)))
 
+;;; Data Utils
+
+(defun decimal-to-binary-list (n)
+  (cond ((= n 0) (list 0))
+        ((= n 1) (list 1))
+        (t (nconc (decimal-to-binary-list (truncate n 2)) (list (mod n 2))))))
+
+(defun ovewrite-lsb (inbyte inbit)
+    (if (eq (mod inbyte 2) 0)
+        (if (eq inbit 0)
+            inbyte 
+            (+ inbyte 1))
+        (if (eq inbit 1)
+            inbyte
+            (- inbyte 1))))
+
+(defun get-lsb (inbyte)
+    (mod inbyte 2))
+
+;;;; File Utils
+
+(defun get-bits-from-file(in list read-counter)
+    (let ((inbyte (read-byte in nil)))
+        (if (eq 0 (mod read-counter (* 1024 1)))
+            (format t "|* Partial parsed message size: ~a bytes ~%" read-counter))
+        (if (not inbyte)
+            list
+            (get-bits-from-file in (append list 
+                (decimal-to-binary-list inbyte)) (+ 1 read-counter)))))
+
+(defun parse-wav-header (in out)
+    (let ((counter  0)
+          (channels ""))
+        (format t "=> Parsing file header ~%")
+        (loop for inbyte = (read-byte in nil) while inbyte do
+            (progn
+                (write-byte inbyte out)
+                (cond 
+                    ((or (eq counter 22) (eq counter 23)) 
+                        (setq channels (concat-string (make-string inbyte) channels)))
+                    ((> counter header-size)
+                        (return-from parse-wav-header 
+                            (parse-integer channels :radix 16))))
+                (+ counter 1)))))
+
+;;; Stego utils
+
+(defun write-op (params)
+    (format t "=> Starting WRITE operation~%")
+    (let ((message (open (get-param :message params) 
+                    :element-type '(unsigned-byte 8)))
+          (original (open (get-param :host params) 
+                    :element-type '(unsigned-byte 8)))
+          (new (open (get-param :result params) 
+                    :element-type '(unsigned-byte 8)
+                    :direction :output 
+                    :if-exists :supersede)))
+        (let ((content (get-bits-from-file message '() 0))
+              (channels-number 0))
+            (close message)
+            (format t "|* Total parsed message size: ~a bytes ~%" (list-length content))
+            (setq channels-number (parse-wav-header original new))
+            (format t "|*Number of channels: ~a ~%" channels-number))
+        (close original)
+        (close new))
+
+(defun read-op (params)
+    (format t "=> Starting READ operation ~%"))
+
 ;;; Entry point
 
 (defun main ()
     (defparameter parsedparams (apply-argv:parse-argv (apply-argv:get-argv)))
-    (validate-params parsedparams))
+    (validate-params parsedparams)
+    (if (get-flag :write parsedparams)
+        (write-op parsedparams)
+        (read-op parsedparams)))
     

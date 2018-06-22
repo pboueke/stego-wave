29978a872d1173c6f1eeb7b4c00f277e7590f949
Pedro Boueke <phboueke@gmail.com>
2018-06-21 23:24:55 -0300

    Write operation done

--- stego-wave.lisp
+++ stego-wave.lisp
@@ -5,7 +5,7 @@
 
 ;;; Globals
 
-(defparameter header-size 64)
+(defparameter *header-size* 64)
 
 ;;; Parameters Utils
 
@@ -35,7 +35,7 @@
         (error "[ERROR] Missing required parameter ~a" param)))
 
 (defun validate-params (params)
-    (format t "|* Parsed parameters: ~a ~%" parsedparams)
+    (format t " * Parsed parameters: ~a ~%" *parsedparams*)
     (validate-operation params)
     (if (get-flag :write params)
         (validate-write-params params)
@@ -65,7 +65,7 @@
 (defun get-bits-from-file(in list read-counter)
     (let ((inbyte (read-byte in nil)))
         (if (eq 0 (mod read-counter (* 1024 1)))
-            (format t "|* Partial parsed message size: ~a bytes ~%" read-counter))
+            (format t " * Partial parsed message size: ~a bytes ~%" read-counter))
         (if (not inbyte)
             list
             (get-bits-from-file in 
@@ -83,9 +83,28 @@
                     ((or (eq counter 22) (eq counter 23))
                         (progn 
                             (setq channels (concatenate 'string (write-to-string inbyte) channels))))
-                    ((> counter header-size)
+                    ((> counter *header-size*)
                         (return-from parse-wav-header (parse-integer channels :radix 16))))
                 (setq counter (+ counter 1))0))))
+
+(defun write-message-bit (byte bit out)
+    (if bit
+        (write-byte (ovewrite-lsb byte bit) out)
+        (write-byte (ovewrite-lsb byte (random 2)) out)))
+        
+
+(defun write-message (message in out channels)
+    (let ((counter  0)
+          (message-bit nil)
+          (message-size (list-length message)))
+        (format t "=> Writing data ~%")
+        (loop for inbyte = (read-byte in nil) while inbyte do
+            (progn
+                (write-message-bit inbyte (car message) out)
+                (setq message (cdr message))
+                (setq counter (+ 1 counter))))
+        (if (> message-size counter) 
+            (error "Message size larger than host capacity"))))
 
 ;;; Stego utils
 
@@ -102,9 +121,10 @@
         (let ((content (get-bits-from-file message '() 0))
               (channels-number 0))
             (close message)
-            (format t "|* Total parsed message size: ~a bytes ~%" (list-length content))
+            (format t " * Total parsed message size: ~a bytes ~%" (list-length content))
             (setq channels-number (parse-wav-header original new))
-            (format t "|*Number of channels: ~a ~%" channels-number))
+            (format t " * Number of channels: ~a ~%" channels-number)
+            (write-message content original new channels-number))
         (close original)
         (close new)))
 
@@ -114,9 +134,9 @@
 ;;; Entry point
 
 (defun main ()
-    (defparameter parsedparams (apply-argv:parse-argv (apply-argv:get-argv)))
-    (validate-params parsedparams)
-    (if (get-flag :write parsedparams)
-        (write-op parsedparams)
-        (read-op parsedparams)))
+    (defparameter *parsedparams* (apply-argv:parse-argv (apply-argv:get-argv)))
+    (validate-params *parsedparams*)
+    (if (get-flag :write *parsedparams*)
+        (write-op *parsedparams*)
+        (read-op *parsedparams*)))
     
